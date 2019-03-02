757aeae8c19b41873238035fc27e8429a1e08ece
unknown <sishack8@gmail.com>
2019-03-02 18:47:19 +0530

    final

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>LaraStart</title>
+  <title>LaraStart | Laravel Admin Dashboard</title>
 
  <link rel="stylesheet" href="/css/app.css">
 </head>
