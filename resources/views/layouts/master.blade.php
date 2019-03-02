7b1d8609f4c2f9719ffc2c052b7d7670682496cd
unknown <sishack8@gmail.com>
2019-03-02 18:45:06 +0530

    Title Changed

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>AdminLTE 3 | Starter</title>
+  <title>LaraStart</title>
 
  <link rel="stylesheet" href="/css/app.css">
 </head>
